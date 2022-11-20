429fe66f2b158aef3c78aee7989fd0df4f7522a2
Jonatan Hutahaean <100518372+Jonatan069@users.noreply.github.com>
2022-11-20 19:29:34 +0700

    Update MataKuliahFactory.php

--- database/factories/MataKuliahFactory.php
+++ database/factories/MataKuliahFactory.php
@@ -20,7 +20,7 @@ class MataKuliahFactory extends Factory
         $matakuliah = ["Algoritma dan Pemrograman", "Struktur Data", "Basis Data", "Pemrograman Berorientasi Objek", "Jaringan Komputer", "Pemrograman Berbasis Web", "Sistem Operasi", "Pemrograman Berbasis Mobile", "Data Mining", "Framework Pemrograman Web", "Machine Learning", "Pengolahan Citra Digital", "Pemrograman Berbasis Mobile", "Pengolahan Citra Digital", "Pengenalan Algoritma dan Pemrograman", "Pengenalan Basis Data", "Algoritma dan Struktur Data", "Statistika dan Probabilitas", "Fisika Dasar", "Sistem Informasi Akuntansi dan Keuangan", "Jaringan dan Keamanan Sistem", "Pemrograman Perangkat Bergerak", "Komputasi Awan", "Pemrograman Animasi dan Multimedia"];
         $faker = Faker::create();
         return [
-            'kode_mk' => $faker -> numerify('IF###'),
+            'kode_mk' => $faker -> unique() -> numerify('IF###'),
             'nama_mk' => $faker -> randomElement($matakuliah),
             'sks' => $faker -> numberBetween(1,4),
             'nama_dosen' => $faker -> name()
